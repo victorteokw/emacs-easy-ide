d75800d9222f0f4bdc74f03ae618b7e9a34a90db
Zhang Kai Yu <yeannylam@gmail.com>
2014-08-07 14:19:18 +0800

    Initial Create Files

---
+++ init.el
@@ -0,0 +1,18 @@
+;;; This config is designed to be an easy IDE.
+
+;;: This config is currently only support emacs 23.
+;;; Code:
+
+(let ((min_ver 24))
+  (unless (>= emacs-major-version min_ver)
+    (error "This config requires v%s or higher" min_ver)))
+
+(add-to-list 'load-path (expand-file-name "user" user-emacs-directory))
+
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+(require 'init-basic)
+
+
+(provide 'init)
+;;; init.el ends here
