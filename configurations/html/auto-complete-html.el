f75d47d65aa412bbdf5edacee6a63308eef6d271
Zhang Kai Yu <yeannylam@gmail.com>
2014-10-26 12:20:40 +0800

    Add powerful ac source to web-mode

--- configurations/html/auto-complete-html.el
+++ configurations/html/auto-complete-html.el
@@ -210,6 +210,29 @@
   (setq-default ac-dwim t)
   )
 
+(defun setup-html-environment-for-web-mode ()
+  "Setup html development environment for web mode."
+  (add-to-list 'ac-modes 'web-mode)
+  ;;(setup-html-environment)
+  (ac-config-default)
+    (setq ac-sources '(ac-source-html-attribute))
+  (add-to-list 'ac-sources 'ac-source-html-tag)
+  (add-to-list 'ac-sources 'ac-source-html-attribute-2)
+
+  (auto-complete-mode t)
+  (setq-default ac-auto-start t)
+  (setq-default ac-dwim t)
+
+  (setq web-mode-ac-sources-alist
+	'(("css" . (ac-source-words-in-buffer ac-source-css-property))
+	  ("html" . (ac-source-html-tag
+		     ac-source-html-attribute
+		     ac-source-html-attribute-2))
+	  ("php" . (ac-source-words-in-buffer
+		    ac-source-words-in-same-mode-buffers
+		    ac-source-dictionary))))
+)
+
 (defun setup-css-environment ()
   "Setup css development environment."
   (require-package 'auto-complete)
@@ -217,6 +240,6 @@
   )
 
 (add-hook 'html-mode-hook 'setup-html-environment)
-(add-hook 'web-mode-hook 'setup-html-environment)
+(add-hook 'web-mode-hook 'setup-html-environment-for-web-mode)
 
 (provide 'auto-complete-html)
