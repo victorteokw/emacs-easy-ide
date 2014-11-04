4f0523cc24f3d4f3f58ae6d28dc9a6989bf8ee43
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-04 23:06:12 +0800

    Add Ruby keyword auto complete

--- configurations/ruby/auto-complete.el
+++ configurations/ruby/auto-complete.el
@@ -4,6 +4,7 @@
   (add-to-list 'ac-modes 'ruby-mode)
   (ac-config-default)
   (setq ac-sources '(ac-source-yasnippet))
+  (add-to-list 'ac-sources 'ac-source-dictionary)
   (auto-complete-mode t)
   (setq-default ac-auto-start t)
   (setq-default ac-dwim t)
