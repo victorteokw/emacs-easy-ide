a2ceb22462bdd0baf5317f3af7101217e5635cbc
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-04 22:51:53 +0800

    Add ruby yasnippet auto complete

---
+++ configurations/ruby/auto-complete.el
@@ -0,0 +1,12 @@
+(defun setup-ruby-auto-complete ()
+  "Set up ruby development auto complete."
+  (require-package 'auto-complete)
+  (add-to-list 'ac-modes 'ruby-mode)
+  (ac-config-default)
+  (setq ac-sources '(ac-source-yasnippet))
+  (auto-complete-mode t)
+  (setq-default ac-auto-start t)
+  (setq-default ac-dwim t)
+  )
+
+(add-hook 'ruby-mode-hook 'setup-ruby-auto-complete)
