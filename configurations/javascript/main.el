73b950c3db7a551cbb0f8c6b90d7afcce18ff44a
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-09 20:26:11 +0800

    add auto complete to js2-mode

--- configurations/javascript/main.el
+++ configurations/javascript/main.el
@@ -1,12 +1,27 @@
 (require-package 'js2-mode)
 (require-package 'helm)
+
+(defun auto-complete-javascript ()
+  (require-package 'auto-complete)
+  (require-package 'ac-js2)
+  (add-to-list 'ac-modes 'js2-mode)
+  (ac-config-default)
+  (setq ac-sources '(ac-source-yasnippet))
+  (auto-complete-mode t)
+  (setq-default ac-auto-start t)
+  (setq-default ac-dwim t)
+  (ac-js2-mode)
+  )
+
 (add-hook 'js2-mode-hook '(lambda ()
                             (js2-imenu-extras-mode)
+
+                            (auto-complete-javascript)
                             ))
 (auto-major-mode 'js2-mode "\\.js\\'")
 (require-package 'json-mode)
 
-(require-package 'ac-js2)
+
 
 ;; Javascript nests {} and () a lot, so I find this helpful
 (require-package 'rainbow-delimiters)
