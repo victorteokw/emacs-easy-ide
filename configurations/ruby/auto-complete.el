5f6c551444cea7479fdc15a5d3ac3830a3ac7fc5
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-13 15:08:54 +0800

    [feature][ruby][enhancement] add back unintelligent auto complete to ruby mode

--- configurations/ruby/auto-complete.el
+++ configurations/ruby/auto-complete.el
@@ -3,7 +3,7 @@
   (require-package 'auto-complete)
   (add-to-list 'ac-modes 'ruby-mode)
   (ac-config-default)
-  (setq ac-sources '(ac-source-yasnippet))
+  (add-to-list 'ac-sources 'ac-source-yasnippet)
   (add-to-list 'ac-sources 'ac-source-dictionary)
   (auto-complete-mode t)
   (setq-default ac-auto-start t)
