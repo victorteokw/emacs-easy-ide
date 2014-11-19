25d9c5f33ddbab0e1e961736aa8b79934e797226
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-19 15:14:02 +0800

    [adjustment] remove rinari and use projectile-rails

--- configurations/ruby/rails.el
+++ configurations/ruby/rails.el
@@ -1,18 +1,45 @@
-(require-package 'rinari)
-(global-rinari-mode 1)
 (require-package 'projectile-rails)
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
-(define-key ruby-mode-map (kbd "C-c f c") 'rinari-find-controller)
-(define-key ruby-mode-map (kbd "C-c f m") 'rinari-find-model)
-(define-key ruby-mode-map (kbd "C-c f v") 'rinari-find-view)
-(define-key ruby-mode-map (kbd "C-c f r") 'rinari-find-rspec)
-(define-key ruby-mode-map (kbd "C-c f t") 'rinari-find-test)
-(define-key ruby-mode-map (kbd "C-c f f") 'rinari-find-fixture)
-(define-key ruby-mode-map (kbd "C-c f r") 'rinari-find-routes)
-(define-key ruby-mode-map (kbd "C-c f a") 'rinari-find-application)
-(define-key ruby-mode-map (kbd "C-c f e") 'rinari-find-environment)
-(define-key ruby-mode-map (kbd "C-c f h") 'rinari-find-helper)
-(define-key ruby-mode-map (kbd "C-c f c") 'rinari-find-stylesheet)
-(define-key ruby-mode-map (kbd "C-c f j") 'rinari-find-javascript)
-(define-key ruby-mode-map (kbd "C-c f n") 'rinari-find-configuration)
+
+(require 'ruby-mode)
+
+;; Find view
+(define-key ruby-mode-map (kbd "C-c f v") 'projectile-rails-find-view)
+(define-key ruby-mode-map (kbd "C-c f V") 'projectile-rails-find-current-view)
+(define-key ruby-mode-map (kbd "C-c f h") 'projectile-rails-find-helper)
+(define-key ruby-mode-map (kbd "C-c f H")
+  'projectile-rails-find-current-helper)
+(define-key ruby-mode-map (kbd "C-c f j") 'projectile-rails-find-javascript)
+(define-key ruby-mode-map (kbd "C-c f J")
+  'projectile-rails-find-current-javascript)
+(define-key ruby-mode-map (kbd "C-c f s") 'projectile-rails-find-stylesheet)
+(define-key ruby-mode-map (kbd "C-c f S")
+  'projectile-rails-find-current-stylesheet)
+
+;; Find controller
+(define-key ruby-mode-map (kbd "C-c f c") 'projectile-rails-find-controller)
+(define-key ruby-mode-map (kbd "C-c f C")
+  'projectile-rails-find-current-controller)
+
+;; Find model
+(define-key ruby-mode-map (kbd "C-c f m") 'projectile-rails-find-model)
+(define-key ruby-mode-map (kbd "C-c f M") 'projectile-rails-find-current-model)
+(define-key ruby-mode-map (kbd "C-c f n") 'projectile-rails-find-migration)
+(define-key ruby-mode-map (kbd "C-c f N")
+  'projectile-rails-find-current-migration)
+
+;; Find test
+(define-key ruby-mode-map (kbd "C-c f p") 'projectile-rails-find-spec)
+(define-key ruby-mode-map (kbd "C-c f P") 'projectile-rails-find-current-spec)
+(define-key ruby-mode-map (kbd "C-c f t") 'projectile-rails-find-test)
+(define-key ruby-mode-map (kbd "C-c f T") 'projectile-rails-find-current-test)
+(define-key ruby-mode-map (kbd "C-c f f") 'projectile-rails-find-fixture)
+(define-key ruby-mode-map (kbd "C-c f F")
+  'projectile-rails-find-current-fixture)
+
+(define-key ruby-mode-map (kbd "C-c f r") 'projectile-rails-goto-routes)
+(define-key ruby-mode-map (kbd "C-c f d") 'projectile-rails-goto-schema)
+
+
+
 
