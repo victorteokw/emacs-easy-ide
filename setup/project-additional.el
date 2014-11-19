e8e8047753141f69b9c19633b5a1ff0c929fea41
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-19 16:16:53 +0800

    [feature] use grizzl with projectile.

--- setup/project-additional.el
+++ setup/project-additional.el
@@ -1,6 +1,14 @@
 (require-package 'projectile)
-
 (projectile-global-mode)
+
+(require-package 'helm)
+(require 'helm-config)
+
+(require-package 'helm-projectile)
+
+(require-package 'grizzl)
+(setq projectile-completion-system 'grizzl)
+
 (require-package 'diminish)
 (diminish 'projectile-mode)
 
