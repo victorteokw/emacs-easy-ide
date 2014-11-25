c8eff5ee65ebd5c5b3c80c1c1e172554f69b24bb
Zhang Kai Yu <yeannylam@gmail.com>
2014-11-25 14:14:54 +0800

    [feature] add .html.erb to html mode

--- configurations/html/main.el
+++ configurations/html/main.el
@@ -1,6 +1,7 @@
 (require-package 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html.erb\\'" . web-mode))
 (setq web-mode-enable-auto-pairing t)
 (setq web-mode-enable-auto-closing t)
 (provide 'init-html)
