91875d489ebabacca2db0517e172c927e2ec23ed
Zhang Kai Yu <yeannylam@gmail.com>
2014-12-25 18:10:23 +0800

    Add Apache show modules

--- configurations/apache/apache.el
+++ configurations/apache/apache.el
@@ -13,10 +13,25 @@
   (apache--with-system 'gnu/linux
                        (find-file "/etc/httpd/conf/httpd.conf")))
 
+(defun apache--execute (command)
+  "Execute command and jump to that buffer."
+  (shell-command command)
+  (switch-to-buffer "*Shell Command Output*"))
+
 ;; See http://httpd.apache.org/docs/2.4/configuring.html
 (defun test-apache-config-file ()
   "Test apache config file."
   (interactive)
-  (shell-command "apachectl configtest") ;; or apachectl -k
-  (switch-to-buffer "*Shell Command Output*"))
+  ;; apachectl configtest or apachectl -k
+  (apache--execute "apachectl configtest"))
+
+(defun apache-show-static-modules ()
+  "Show apache's modules that are currently compiled into the server."
+  (interactive)
+  (apache--execute "apachectl -l"))
+
+(defun apache-show-dynamic-modules ()
+  "Show apache's modules that are loaded dynaically."
+  (interactive)
+  (apache--execute "apachectl -M"))
 
