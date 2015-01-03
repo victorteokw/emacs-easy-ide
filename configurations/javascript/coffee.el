5da296ab93f79c624526b31081d472acb49ae4fb
Zhang Kai Yu <yeannylam@gmail.com>
2015-01-03 21:03:13 +0800

    Coffee script auto completes.

--- configurations/javascript/coffee.el
+++ configurations/javascript/coffee.el
@@ -1,4 +1,6 @@
 (require-package 'coffee-mode)
 (add-hook 'coffee-mode-hook
           (lambda ()
-            (add-hook 'before-save-hook 'whitespace-cleanup)))
+            (add-hook 'before-save-hook 'whitespace-cleanup)
+            (auto-complete-mode)
+            ))
