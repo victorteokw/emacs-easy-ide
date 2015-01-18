ccdd61572a18b2b0ea5b2efbb94d4df3d405ea3a
Zhang Kai Yu <yeannylam@gmail.com>
2015-01-18 10:39:47 +0800

    Add some handy functions.

--- configurations/html/main.el
+++ configurations/html/main.el
@@ -8,4 +8,42 @@
 (setq web-mode-enable-auto-pairing t)
 (setq web-mode-enable-auto-closing t)
 
+(defun hoiyu--rubify-and-translate ()
+  "Translate."
+  (interactive)
+  (let ((start (1+ (save-excursion
+                     (search-backward ">" nil t))))
+        (end (1- (save-excursion
+                   (search-forward "<" nil t))))
+        content)
+    (and start end (progn
+                     (setq content (read-string "t: "))
+                     (delete-region start end)
+                     (goto-char start)
+                     (insert "<%= t(\"")
+                     (insert content)
+                     (insert "\") %>")
+                     (goto-char (- (point) 5))
+                     ))))
+
+(defun hoiyu--translate-ruby-string ()
+  "Translate."
+  (interactive)
+  (let ((start (1+ (save-excursion
+                     (re-search-backward "[^\\\\]'"))))
+        (end (save-excursion
+               (re-search-forward "[^\\]'")))
+        content)
+    (setq content (buffer-substring-no-properties start end))
+    (setq content (downcase content))
+    (setq content (replace-regexp-in-string " " "_" content))
+    (setq content (replace-regexp-in-string "^'" "" content))
+    (setq content (replace-regexp-in-string "'$" "" content))
+    (setq content (read-string "t: " content nil content))
+    (delete-region start end)
+    (insert "t(\"")
+    (insert content)
+    (insert "\")")
+    ))
+
 (provide 'init-html)
