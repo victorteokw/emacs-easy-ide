ee59eab18946ca1bc1ab441fdbcfbd77c39bd7a7
Zhang Kai Yu <yeannylam@gmail.com>
2015-09-14 11:58:50 +0800

    Paredit delete react to region

--- conf/eide-elisp.el
+++ conf/eide-elisp.el
@@ -1,48 +1,68 @@
-;; Emacs lisp
+;;;; Emacs lisp
 
-;; .el file loading
+;;; .el or .elc just loading new one
+
 (setq load-prefer-newer t)
 
-;; open Cask file in elisp-mode
+;;; open Cask file in elisp-mode
+
 (add-to-list 'auto-mode-alist '("Cask" . emacs-lisp-mode))
 
-;; expand macro
+;;; expand macro
 
 ;; (require 'macrostep)
 
-;; Code folding
+;;; Code folding
 
 (add-hook 'emacs-lisp-mode-hook 'hs-minor-mode)
 
-(defun hy-lang-elisp-click-to-jump (event)
-  "you know"
+;;; Jump to definition
+
+(defun eide-elisp-click-to-jump (event)
+  "Click to jump to definition."
   (interactive "e")
   (select-window (posn-window (event-start event)))
   (goto-char (posn-point (event-start event)))
   (elisp-slime-nav-find-elisp-thing-at-point (elisp-slime-nav--read-symbol-at-point)))
 
-(defun ky/elisp-common-setup ()
-  "Elisp common setup."
-  ;; jump
-  (turn-on-elisp-slime-nav-mode)
-  (define-key elisp-slime-nav-mode-map [M-mouse-1] 'hy-lang-elisp-click-to-jump)
-  ;; rainbow parens
-  (rainbow-delimiters-mode)
-  ;; editing
-  (paredit-mode)
-  (define-key paredit-mode-map (kbd "M-<left>") 'paredit-wrap-sexp)
-  ;; identation
-  (add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode)
-  ;; highlight sexp
-  ;; (add-hook 'emacs-lisp-mode-hook 'hl-sexp-mode)
-  ;; short documentation
-  (eldoc-mode)
-  ;; show parens
-  (show-paren-mode)
-  ;; yasnippet
-  (yas-minor-mode)
+(require 'elisp-slime-nav)
 
-  ;; auto complete
+(define-key elisp-slime-nav-mode-map [M-mouse-1] 'eide-elisp-click-to-jump)
+
+(add-hook 'emacs-lisp-mode-hook 'turn-on-elisp-slime-nav-mode)
+
+;;; Rainbow delimiters
+
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+
+;;; Paredit mode
+
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+
+(require 'paredit)
+
+(define-key paredit-mode-map (kbd "M-<left>") 'paredit-wrap-sexp)
+(define-key paredit-mode-map (kbd "M-<right>") 'paredit-wrap-sexp)
+
+;;; Aggressive indent
+
+(add-hook 'emacs-lisp-mode-hook 'aggressive-indent-mode)
+
+;;; Yasnippet
+
+(add-hook 'emacs-lisp-mode-hook 'yas-minor-mode)
+
+;;; Eldoc
+
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+
+;;; Show paren mode
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;;; Auto complete
+
+(defun eide-elisp-auto-complete ()
   (require 'auto-complete)
   (require 'auto-complete-config)
   (setq ac-ignore-case nil)
@@ -52,30 +72,47 @@
                      ac-source-functions
                      ac-source-symbols
                      ac-source-variables
-                     ac-source-words-in-same-mode-buffers
-                     ))
-  (auto-complete-mode t))
+                     ac-source-words-in-same-mode-buffers))
+  (auto-complete-mode))
 
-(defun ky/elisp-spec-setup ()
-  "Elisp editing setup."
-  ;; clean whitespace
-  (add-hook 'before-save-hook 'whitespace-cleanup)
-  ;; check parents when saving
+(add-hook 'emacs-lisp-mode-hook 'eide-elisp-auto-complete)
+
+;;; Check parens
+
+(defun eide-elisp-check-parens ()
   (add-hook 'after-save-hook #'check-parens nil t))
 
-(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-  (add-hook hook 'ky/elisp-common-setup))
+(add-hook 'emacs-lisp-mode-hook 'eide-elisp-check-parens)
 
-(add-hook 'emacs-lisp-mode-hook 'ky/elisp-spec-setup)
+;;; Clean whitespaces
 
-;; Flycheck elisp
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (flycheck-mode)
-            (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
-            ))
+(defun eide-elisp-clean-whitespaces ()
+  (add-hook 'before-save-hook 'whitespace-cleanup))
 
-;; eshell
+(add-hook 'emacs-lisp-mode-hook 'eide-elisp-clean-whitespaces)
+
+;;; Delete should react to region
+
+(defadvice paredit-backward-delete (around eide-paredit-delete-region activate)
+  (if mark-active
+      (delete-backward-char 1)
+    ad-do-it))
+
+(defadvice paredit-forward-delete (around eide-paredit-delete-region activate)
+  (if mark-active
+      (delete-forward-char 1)
+    ad-do-it))
+
+;;; Flycheck elisp
+
+(defun eide-elisp-flycheck ()
+  (flycheck-mode)
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
+
+(add-hook 'emacs-lisp-mode-hook 'eide-elisp-flycheck)
+
+;;; eshell
+
 (eval-after-load
     "eshell"
   '(setq eshell-directory-name (f-expand "etc/eshell" eide-etc-dir)))
